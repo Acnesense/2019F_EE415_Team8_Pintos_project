67d212b1921408cd0b6e682fc7ae6c39bb1a292e
Choi Donghyeok <ehdgur3125@naver.com>
2019-09-21 19:25:12 +0900

    d

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -92,9 +92,8 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  thread_sleep( start + ticks );
-  //while (timer_elapsed (start) < ticks) 
-  //  thread_yield ();
+  while (timer_elapsed (start) < ticks) 
+    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -173,7 +172,6 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
-  thread_awake(ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
